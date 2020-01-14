a5420a0ff273c6400f98a94c5e9e4dd53b719480
Michal Budzowski <michal.budzowski@sonarsoruce.com>
2020-01-14 16:41:47 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static String MyValue = "moro";
+        static void Main(string[] args)
+        {
+            MyValue.CompareTo(args == null ? args[0] : null);
+            (args == null ? args[0] : null).CompareTo(MyValue);
+        }
+    }
+}
